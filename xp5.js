b26166a203558aed5c25c559a781a5406c8ab767
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2017-11-29 17:34:55 +1100

    xp5.js

---
+++ xp5.js
@@ -0,0 +1,25 @@
+var syntax = "name(content);";
+var endednum;
+boot();
+thisscript();
+
+function thisscript() {
+	var x;
+	var y;
+	var pos;
+	var rect;
+	var circle;
+	var draw;
+	var showvar;
+	end();
+}
+var x = 0;
+var y = 0;
+function boot() {
+	alert("Made With xp5 Framework");
+	end();
+}
+function end() {
+	console.log("Ended");
+	endednum++;
+}
